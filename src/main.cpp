6c3a03cf54dbdc0fde6feba72489b03122bb8f15
Vlad Gheorghiu <vgheorgh@gmail.com>
2014-08-01 17:59:15 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,46 @@
 #include "token.h"
 
 
-template<typename T> 
-void displayContainer(const T& container)
+template<typename T>
+void displayContainer(const T &container)
 {
-    for(typename T::const_iterator it = container.begin(); 
-        it != container.end(); it++)
-        	std::cout << "[" << *it << "]" << " ";    
+    for (typename T::const_iterator it = container.begin();
+            it != container.end(); it++)
+        std::cout << "[" << *it << "]" << " ";
 }
 
-void displayTextFile(const char* fname)
+void displayTextFile(const char *fname)
 {
     std::string tmp;
     std::ifstream file(fname);
-    while(std::getline(file,tmp))
+    while (std::getline(file, tmp))
         std::cout << tmp << std::endl;
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    if(argc!=3)
+    if (argc != 3)
     {
         std::cout << "Need 2 arguments: filename and delimiters"\
-		      	  << std::endl;
+                  << std::endl;
         return 1;
-    }    
+    }
     std::ifstream file(argv[1]); // input file stream
-    if(!file.is_open())
-		throw std::runtime_error("File not found!");
+    if (!file.is_open())
+        throw std::runtime_error("File not found!");
 
-    std::string delim(argv[2]); // delimiter 
+    std::string delim(argv[2]); // delimiter
 
     std::cout << "The original file is: " << std::endl;
     displayTextFile(argv[1]);
-    
+
     std::cout << std::endl << "The tokanized file is: " << std::endl;
     std::string tmp;
-    while(std::getline(file, tmp)) // tokenize each line
+    while (std::getline(file, tmp)) // tokenize each line
     {
         displayContainer<std::vector<std::string> >(split(tmp, delim));
         std::cout << std::endl;
-    }   
-    
-    return 0; 
+    }
+
+    return 0;
 }
