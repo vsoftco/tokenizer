42266d5d8d71bb42585f064df6bc47e2530f264d
Vlad Gheorghiu <vsoftco@gmail.com>
2016-10-16 23:02:31 -0400

    Switch to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,32 @@
-#include <cstdlib>
-#include <fstream>
 #include <iostream>
-#include <stdexcept>
 #include <string>
-#include "token.h"
+#include "split.h"
 
 template<typename T>
-void displayContainer(const T &container)
+void disp(const T& container)
 {
+    std::cout << "[";
+    bool first = true;
     for (typename T::const_iterator it = container.begin();
             it != container.end(); ++it)
-        std::cout << "[" << *it << "]" << " ";
+    {
+        if(first)
+        {
+            std::cout << *it;
+            first = false;
+        }
+        else
+        {
+            std::cout << " " << *it;
+        }
+    }
+    std::cout << "]" << std::endl;
 }
 
-void displayTextFile(const std::string& fname)
+int main()
 {
-	std::ifstream fin(fname.c_str());
-	if(!fin)
-		throw std::runtime_error("Cannot open input file \"" + fname + "\"");
-	std::cout << fin.rdbuf();
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        std::cout << "Need 2 arguments: \"filename\" and \"delimiters\""\
-                  << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-    std::ifstream fin(argv[1]); // input file stream
-    if (!fin)
-        throw std::runtime_error("Cannot open input file \""\
-			   + std::string(argv[1]) + "\"");
-
-    std::string delim(argv[2]); // delimiter
-
-    std::cout << "The original file is: " << std::endl;
-    displayTextFile(argv[1]);
-
-    std::cout << std::endl << "The tokenized file is: " << std::endl;
-    std::string tmp;
-    while (std::getline(fin, tmp)) // tokenize each line
-    {
-        displayContainer(split(tmp, delim));
-        std::cout << std::endl;
-    }
-
-    return 0;
+    std::string str1 = "This is a test";
+    disp(split(str1));        // by default splits on white spaces
+    std::string str2 = "This,is:some:other;test";
+    disp(split(str2, ",:;")); // custom split delimiters
 }
