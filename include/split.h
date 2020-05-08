eb8d0648a8fa3eca6256e47ac67b5c4326815032
Vlad Gheorghiu <vsoftco@gmail.com>
2020-05-08 11:39:20 -0400

    prettyprint

--- include/split.h
+++ include/split.h
@@ -1,12 +1,12 @@
 #ifndef SPLIT_H_
 #define SPLIT_H_
 
-#include <vector>
 #include <string>
+#include <vector>
 
 // tokenizer, standard C++98
 // white spaces as default delimiter
-std::vector<std::string> 
-split(const std::string& str, const std::string& delim = " \t\n"); 
+std::vector<std::string> split(const std::string& str,
+                               const std::string& delim = " \t\n");
 
 #endif // SPLIT_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
+#include "split.h"
 #include <iostream>
 #include <string>
-#include "split.h"
 
-template<typename T>
-void disp(const T& container)
-{
+template <typename T>
+void disp(const T& container) {
     for (typename T::const_iterator it = container.begin();
-            it != container.end(); ++it)
-    {
+         it != container.end(); ++it) {
         std::cout << '[' << *it << "] ";
     }
 }
 
-int main()
-{
+int main() {
     std::string str1 = "This is a test";
     std::cout << "Tokens: ";
-    disp(split(str1));        // by default splits on white spaces
+    disp(split(str1)); // by default splits on white spaces
     std::cout << '\n';
 
     std::string str2 = "This, is:some:other;test";
